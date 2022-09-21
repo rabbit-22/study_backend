afe143090d117d1a2103a6866133697d251bc52b
rabbit-22 <chlwlal20@gmail.com>
2022-09-21 14:24:24 +0000

    feat: complete all api of board

--- controllers/board.js
+++ controllers/board.js
@@ -1,21 +1,74 @@
 const db = require('../models');
 
 const getPosts = async (req, res) => {
-    const posts = await db.Post.findAll();
-    res.status(200).send(posts);
+    try {
+        const posts = await db.Post.findAll();
+        res.status(200).send(posts);
+    } catch (err) {
+        res.status(err.status || 500).send(err.message);
+    }
 };
 
 const getPost = async (req, res) => {
-    var id = req.params['id'];
-    const post = await db.Post.findOne({
-        where : { id : id }
-    });
-    res.status(200).send(post);
+    try {
+        const id = req.params['id'];
+        const post = await db.Post.findOne({
+            where : { id : id }
+        });
+        res.status(200).send(post);
+    } catch (err) {
+        res.status(err.status || 500).send(err.message);
+    }
+};
+
+const createPost = async (req, res) => {
+    try {
+        const post = {
+            title: req.body.title,
+            content: req.body.content,
+        };
+        console.log(req.body);
+        const result = await db.Post.create(post);
+        res.status(201).send(result);
+    } catch (err) {
+        res.status(err.status || 500).send(err.message);
+    }
+};
+
+const updatePost = async (req, res) => {
+    try {
+        const id = req.params['id'];
+        const post = {
+            title: req.body.title,
+            content: req.body.content,
+        };
+        const result = await db.Post.update(post, {
+            where : { id : id }
+        });
+        res.status(201).send(result);
+    } catch (err) {
+        res.status(err.status || 500).send(err.message);
+    }
+};
+
+const deletePost = async (req, res) => {
+    try {
+        const id = req.params['id'];
+        db.Post.destroy({
+            where : { id : id }
+        });
+        res.status(204).send();
+    } catch (err) {
+        res.status(err.status || 500).send(err.message);
+    }
 };
 
 module.exports = {
     getPosts,
-    getPost
+    getPost,
+    createPost,
+    updatePost,
+    deletePost,
 };
 
 
--- routes/board.js
+++ routes/board.js
@@ -2,8 +2,14 @@ var express = require('express');
 const board = require('../controllers/board');
 var router = express.Router();
 
-router.get('/all', board.getPosts);
+router.get('/', board.getPosts);
 
 router.get('/:id', board.getPost);
+
+router.post('/', board.createPost);
+
+router.put('/:id', board.updatePost);
+
+router.delete('/:id', board.deletePost);
 
 module.exports = router;
